c5dca37c30c31b27e9cb4bc4d2cc600bd3fcb74d
Nonzulu <knzulu16@gmail.com>
2017-05-22 14:21:10 +0200

    save changes made

--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -1,7 +1,24 @@
 var mostProfitableDepartment=require('../mostProfitableDepartment');
 const assert=require('assert');
+var salesData = [
+    {department : 'hardware', sales : 4500, day : 'Monday'},
+    {department : 'outdoor', sales : 1500, day : 'Monday'},
+    {department : 'carpentry', sales : 5500, day : 'Monday'},
+    {department : 'hardware', sales : 7500, day : 'Tuesday'},
+    {department : 'outdoor', sales : 2505, day : 'Tuesday'},
+    {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+    {department : 'hardware', sales : 1500, day : 'Wednesday'},
+    {department : 'outdoor', sales : 8507, day : 'Wednesday'},
+    {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+    {department : 'hardware', sales : 12000, day : 'Thursday'},
+    {department : 'outdoor', sales : 18007, day : 'Thursday'},
+    {department : 'carpentry', sales : 6109, day : 'Thursday'},
+    {department : 'hardware', sales : 7005, day : 'Friday'},
+    {department : 'outdoor', sales : 12006, day : 'Friday'},
+    {department : 'carpentry', sales : 16109, day : 'Friday'},
+];
 describe('The most profitable department function',function(){
 it('should return the most profitable department',function(){
-  assert.equal(mostProfitableDepartment('outdoor'),'');
+  assert.equal(mostProfitableDepartment(salesData),'');
     });
 });
